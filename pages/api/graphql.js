1ba874fbed965e14f4646e2ebc824749202b3566
WillHDev <william.holcombe@uconn.edu>
2019-11-30 15:58:45 -0700

    connected to mongodb

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
-import '../../lib/Mongoose';
+import connectDb from '../../lib/Mongoose';
 
 const typeDefs = gql`
 type Query {
@@ -23,4 +23,5 @@ export const config  = {
     }
 }
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+const server =  apolloServer.createHandler({ path: "/api/graphql" });
+export default connectDb(server);
